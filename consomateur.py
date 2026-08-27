7a802ef74ec7977ec8289caa34b2846e57ef1d73
wbh6813 <walid_hammoud@hotmail.fr>
2026-08-27 14:22:42 +0000

    modif

--- consomateur.py
+++ consomateur.py
@@ -1,23 +1,8 @@
-from airflow.decorators import dag, task
-from airflow.datasets import Dataset
 from datetime import datetime
-
-data_file = Dataset("/tmp/dataset_asset.txt")
-
-
-@dag(
-    dag_id="consommateur_asset_bash",
-    start_date=datetime(2026, 1, 1),
-    schedule=[data_file],
-    catchup=False,
-)
-def consommateur_asset_bash():
-
-    @task.bash
-    def lire_fichier():
-        return "cat /tmp/dataset_asset.txt"
-
-    lire_fichier()
+from airflow.sdk import DAG, Asset
+from airflow.operators.bash import BashOperator
 
 
-consommateur_dag = consommateur_asset_bash()
+mon_fichier = Asset("file:///tmp/mon_asset.txt")
+
+
--- producteur.py
+++ producteur.py
@@ -1,28 +1,36 @@
 from datetime import datetime
 
-from airflow import DAG
-from airflow.decorators import dag
-from airflow.datasets import Dataset
-from airflow.operators.bash import BashOperator
+from airflow.sdk import DAG, Asset
+from airflow.providers.standard.operators.bash import BashOperator
 
 
-data_file = Dataset("file:///tmp/dataset.txt")
+mon_asset = Asset("file:///tmp/mon_asset.txt")
 
 
-@dag(
-    dag_id="producteur_dataset_bash",
-    start_date=datetime(2026, 2, 20),
+# DAG PRODUCTEUR
+with DAG(
+    dag_id="producteur_asset_bash",
+    start_date=datetime(2026, 1, 1),
     schedule=None,
     catchup=False,
-)
-def producteur():
+) as dag:
 
     produire_fichier = BashOperator(
         task_id="produire_fichier",
-        bash_command="echo 'hello' > /tmp/dataset.txt",
-        outlets=[data_file],
+        bash_command="echo 'hello' > /tmp/mon_asset.txt",
+        outlets=[mon_asset],
     )
 
-    write_to_file()
 
-producteur_dag = producteur()
+# DAG CONSOMMATEUR
+with DAG(
+    dag_id="consommateur_asset_bash",
+    start_date=datetime(2026, 1, 1),
+    schedule=[mon_asset],
+    catchup=False,
+) as dag:
+
+    lire_fichier = BashOperator(
+        task_id="lire_fichier",
+        bash_command="cat /tmp/mon_asset.txt",
+    )
